f0af2ce40ca2212b6f7c13818038fc16be9ba805
rohit peesa <29476002+rohitpeesa@users.noreply.github.com>
2022-09-27 09:21:50 -0400

    Update pdf downloader.py

--- pdf downloader.py
+++ pdf downloader.py
@@ -7,21 +7,21 @@ def download_pdf(url, file_name, headers):
     r = requests.get(url, headers=headers)
     # Save the PDF
     if r.status_code == 200:
-        with open(r"C:\Users\rohit\Downloads\Capstone Data\\" + file_name + ".pdf", 'wb') as f:
+        with open(r"C:\Users\rohit\Downloads\Data\\" + file_name + ".pdf", 'wb') as f:
             f.write(r.content)
     else:
         print(response.status_code)
 
 
 if __name__ == "__main__":
-    df = pd.read_excel('Official Statement Links (Private Higher Education, Hospitals, CA Land).xlsx',sheet_name="Hospitals")
+    df = pd.read_excel('assignment.xlsx',sheet_name="Rating")
     row_count = len(df)
     headers = {
             "User-Agent": "Chrome/51.0.2704.103",
         }
     for i in range(0,row_count):
-        url=df['HyperLink'][i]
-        file_name=df['Issuer Name'][i]
+        url=df['Rating Links'][i] #links column in excel
+        file_name=df['Rating Issuer'][i] #Column to name the files based on who issued the ratings
         try:
             download_pdf(url, file_name.replace('/',"_").replace('"',""), headers)
         except Exception as e:
